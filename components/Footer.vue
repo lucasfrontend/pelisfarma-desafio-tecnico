fcf0da939eb6ef933878ea1b93237c74dcdaebdd
lucasfrontend <lucasmartinezguardavidas@gmail.com>
2023-05-24 14:29:40 -0300

    color footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,23 +3,23 @@
     <v-container>
         <v-row>
         <v-col cols="12" md="3">
-            <h3 class="white--text">Nosotros</h3>
-            <p class="white--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo laborum nisi deserunt laudantium sit est incidunt quis.</p>
+            <h class="white--text">Nosotros</h>
+            <p class="grey--text text--darken-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo laborum nisi deserunt laudantium sit est incidunt quis.</p>
         </v-col>
         <v-col cols="12" md="3">
-            <h3 class="white--text">Terms of Use</h3>
-            <a class="white--text" href="#">Términos y condiciones</a>
-            <a class="white--text" href="#">Acceptable Use Policy</a>
+            <h4 class="white--text">Terms of Use</h4>
+            <a class="grey--text text--darken-2" href="#">Términos y condiciones</a>
+            <a class="grey--text text--darken-2" href="#">Acceptable Use Policy</a>
         </v-col>
         <v-col cols="12" md="3">
-            <h3 class="white--text">Políticas de Privacidad</h3>
-            <a class="white--text" href="#">Protección de datos</a>
-            <a class="white--text" href="#">Política de cookies</a>
+            <h4 class="white--text">Políticas de Privacidad</h4>
+            <a class="grey--text text--darken-2" href="#">Protección de datos</a>
+            <a class="grey--text text--darken-2" href="#">Política de cookies</a>
         </v-col>
         <v-col cols="12" md="3">
-            <h3 class="white--text">FAQ</h3>
-            <a class="white--text" href="#">Preguntas frecuentes</a>
-            <a class="white--text" href="#">Soporte</a>
+            <h4 class="white--text">FAQ</h4>
+            <a class="grey--text text--darken-2" href="#">Preguntas frecuentes</a>
+            <a class="grey--text text--darken-2" href="#">Soporte</a>
         </v-col>
         </v-row>
     </v-container>
@@ -27,7 +27,6 @@
         <v-row>
         <v-col cols="12" class="text-right">
             <div>Verifarma &copy; {{ new Date().getFullYear() }}</div>
-            <img :src="bottomLogo" alt="Verifarma"> 
         </v-col>
         </v-row>
     </v-container>
